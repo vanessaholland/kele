2bcf112c91fe3a48c6b42b946f3eb9f2dcd8caa1
vanessa holland <vanessaholland@legalshieldcorp.com>
2017-02-15 11:10:45 -0600

    user story 7 completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -22,30 +22,67 @@ class Kele
   end
 
   def get_me
-    response = self.class.get(base_url('users/me'), headers: { "authorization" => @auth_token })
+    headers = {
+      headers: {
+        :content_type => 'application/json',
+        :authorization => @auth_token
+      }
+    }
+    response = self.class.get(base_url('users/me'), headers)
     return @user_data = JSON.parse(response.body)
   end
 
   def get_mentor_availability(mentor_id)
-    response = self.class.get(base_url("mentors/#{mentor_id}/student_availability"), headers: { "content_type" => 'application/json', "authorization" => @auth_token })
+    headers = {
+      headers: {
+        :content_type => 'application/json',
+        :authorization => @auth_token
+      }
+    }
+    response = self.class.get(base_url("mentors/#{mentor_id}/student_availability"), headers)
     return @mentor_avail = JSON.parse(response.body)
   end
 
   def get_messages(page = 1)
-    response = self.class.get(base_url("message_threads"), values: {"page": page}, headers: { "content_type" => 'application/json', "authorization" => @auth_token })
+    response = self.class.get(base_url("message_threads"), values: {"page": page} , headers: { "content_type" => 'application/json', "authorization" => @auth_token })
     return @messages = JSON.parse(response.body)
   end
 
   def create_message(sender, recipient_id, token, subject, stripped_text, prod)
-    values = '{
+    values = {
         "sender": sender,
         "recipient_id": recipient_id,
         "token": token,
         "subject": subject,
         "stripped-text": stripped_text
-        }'
-        response = self.class.post(base_url("messages", prod), body: values, headers: { "authorization" => @auth_token })
-        puts response
+    }
+    headers = {
+        :content_type => 'application/json',
+        :authorization => @auth_token
+    }
+
+    response = self.class.post(base_url("messages", prod), body: values, headers: headers)
+    raise "There was an error submitting this message" if response.code != 200
+    puts response
+  end
+
+  def create_submission(assignment_branch, assignment_commit_link, checkpoint_id, comment, enrollment_id, prod)
+    headers = {
+        :content_type => 'application/json',
+        :authorization => @auth_token
+    }
+
+    values = {
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "checkpoint_id": checkpoint_id,
+        "comment": comment,
+        "enrollment_id": enrollment_id
+    }
+
+    response = self.class.post(base_url("checkpoint_submissions", prod), body: values, headers: headers)
+    raise "There was a problem processing this submission" if response.code != 200
+    puts response
   end
 
   private
